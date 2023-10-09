39bfe4cab81bc4926959414155325e46f6a81316
tofiq7770 <tofigtn@code.edu.az>
2023-10-10 00:38:18 +0400

    Adding Files

---
+++ Ab104-09/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+string input = "        EverytHing  i neeD,    is a CuP   of       CoFFee    ";
+
+input = input.Trim();
+
+string[] words = input.Split(' ');
+
+words = words.Where(word => !string.IsNullOrWhiteSpace(word)).ToArray();
+
+StringBuilder correctPhrase = new StringBuilder();
+
+foreach (string word in words)
+{
+    if (correctPhrase.Length > 0)
+    {
+        correctPhrase.Append(' ');
+    }
+    correctPhrase.Append(char.ToUpper(word[0]));
+    correctPhrase.Append(word.Substring(1).ToLower());
+}
+
+Console.WriteLine(correctPhrase.ToString());
